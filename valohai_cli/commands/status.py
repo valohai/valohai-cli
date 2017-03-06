ff16e69738cea7863f8fa96d58359989f12214ba
Aarni Koskela <akx@iki.fi>
2017-03-06 17:50:30 +0200

    Add basic `status` command

---
+++ valohai_cli/commands/status.py
@@ -0,0 +1,54 @@
+import click
+
+from valohai_cli.api import request
+from valohai_cli.ctx import get_project
+from valohai_cli.messages import print_table
+
+
+@click.command()
+@click.option('--summary/--no-summary', default=True, help='Show execution summary')
+@click.option('--incomplete/--no-incomplete', default=True, help='Show details of incomplete executions')
+def status(summary, incomplete):
+    """
+    Get the general status of the linked project
+    """
+    project = get_project(require=True)
+    project_data = request('get', '/api/v0/projects/{id}/'.format(id=project.id)).json()
+
+    click.secho('# Project %s\n' % click.style(project.name, underline=True), bold=True)
+
+    if summary:
+        print_execution_summary(project_data)
+    if incomplete:
+        print_incomplete_executions(project)
+
+
+def print_incomplete_executions(project):
+    incomplete_executions = request('get', '/api/v0/executions/', params={
+        'project': project.id,
+        'status': 'incomplete',
+        'ordering': 'counter',
+    }).json().get('results', ())
+
+    click.secho('## %d Incomplete Executions\n' % len(incomplete_executions), bold=True)
+
+    print_table(incomplete_executions, ['counter', 'status', 'step'], headers=['#', 'Status', 'Step'])
+
+
+def print_execution_summary(project_data):
+    execution_summary = project_data.get('execution_summary', {}).copy()
+    if not execution_summary:
+        return
+    total = execution_summary.pop('count')
+    click.secho('## Summary of %d executions\n' % total, bold=True)
+    print_table(
+        [
+            {'status': key.replace('_count', ''), 'count': value}
+            for (key, value)
+            in sorted(execution_summary.items())
+            if value
+        ],
+        columns=('status', 'count'),
+        headers=('Status', 'Count'),
+    )
+    click.secho('\n')
