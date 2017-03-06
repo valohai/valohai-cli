78b09e6e09264031be51456cef84bfb4101ab951
Aarni Koskela <akx@iki.fi>
2017-03-06 21:18:15 +0200

    Add run command

---
+++ tests/test_run.py
@@ -0,0 +1,91 @@
+import json
+
+import pytest
+import requests_mock
+
+from tests.conftest import TEST_PROJECT_DATA
+from valohai_cli import git
+from valohai_cli.commands.run import run
+from valohai_cli.ctx import get_project
+
+CONFIG_YAML = """
+---
+
+- step:
+    name: Train model
+    image: busybox
+    command: "false"
+    inputs:
+      - name: in1
+        default: http://example.com/
+    parameters:
+      - name: max_steps
+        pass-as: --max_steps={v}
+        description: Number of steps to run the trainer
+        type: integer
+        default: 300
+
+"""
+
+
+def get_test_run_requests_mock(project_id, commit_id, additional_payload_values):
+    m = requests_mock.mock()
+    m.get('https://app.valohai.com/api/v0/projects/{}/commits/'.format(project_id), json=[
+        {
+            'identifier': commit_id,
+        },
+    ])
+
+    def handle_create_execution(request, context):
+        body_json = json.loads(request.body.decode('utf-8'))
+        assert body_json['project'] == project_id
+        assert body_json['step'] == 'Train model'
+        assert body_json['commit'] == commit_id
+        for key, value in additional_payload_values.items():
+            assert body_json[key] == value
+        context.status_code = 201
+        return {
+            'id': 1337,
+            'counter': 1337,
+            'link': '/',
+        }
+
+    m.post('https://app.valohai.com/api/v0/executions/', json=handle_create_execution)
+
+    return m
+
+
+def test_run_requires_step(runner, logged_in_and_linked):
+    assert 'Missing argument' in runner.invoke(run, catch_exceptions=False).output
+
+
+@pytest.mark.parametrize('pass_param', (False, True))
+@pytest.mark.parametrize('pass_input', (False, True))
+def test_run(runner, logged_in_and_linked, monkeypatch, pass_param, pass_input):
+    project_id = TEST_PROJECT_DATA['id']
+    commit_id = 'f' * 40
+    monkeypatch.setattr(git, 'get_current_commit', lambda dir: commit_id)
+
+    with open(get_project().get_config_filename(), 'w') as yaml_fp:
+        yaml_fp.write(CONFIG_YAML)
+
+    args = ['train']
+    values = {}
+    if pass_param:
+        args.append('--max-steps=1801')
+        values['parameters'] = {'max_steps': 1801}
+
+    if pass_input:
+        args.append('--in1=http://url')
+        values['inputs'] = {'in1': 'http://url'}
+
+    with get_test_run_requests_mock(project_id, commit_id, values):
+        output = runner.invoke(run, args, catch_exceptions=False).output
+        assert '#1337' in output
+
+
+def test_param_type_validation(runner, logged_in_and_linked):
+    with open(get_project().get_config_filename(), 'w') as yaml_fp:
+        yaml_fp.write(CONFIG_YAML)
+    rv = runner.invoke(run, ['train', '--max-steps=plonk'], catch_exceptions=False)
+    assert 'plonk is not a valid integer' in rv.output
---
+++ valohai_cli/commands/run.py
@@ -0,0 +1,171 @@
+import click
+from click.exceptions import BadParameter
+from valohai_yaml.objs.parameter import Parameter
+from valohai_yaml.objs.step import Step
+from valohai_yaml.objs.input import Input
+
+from valohai_cli.api import request
+from valohai_cli.ctx import get_project
+import valohai_cli.git as git  # this import style required for tests
+from valohai_cli.messages import warn, success
+from valohai_cli.utils import match_prefix, humanize_identifier
+
+
+# TODO: this should use the step config from an overridden commit
+
+class RunCommand(click.Command):
+    """
+    A dynamically-generated subcommand that has Click options for parameters and inputs.
+    """
+    parameter_type_map = {
+        'integer': click.INT,
+        'float': click.FLOAT,
+    }
+
+    def __init__(self, project, step, commit, environment):
+        """
+        Initialize the dynamic run command.
+
+        :param project: Project object
+        :type project: valohai_cli.ctx.Project
+        :param step: YAML step object
+        :type step: valohai_yaml.objs.Step
+        :param commit: Commit identifier
+        :type commit: str
+        :param environment: Environment name
+        :type environment: str
+        """
+        assert isinstance(step, Step)
+        self.project = project
+        self.step = step
+        self.commit = commit
+        self.environment = environment
+        super(RunCommand, self).__init__(
+            name=step.name.lower().replace(' ', '-'),
+            callback=self.execute,
+            add_help_option=True,
+        )
+        for parameter in step.parameters.values():
+            self.params.append(self.convert_param_to_option(parameter))
+        for input in step.inputs.values():
+            self.params.append(self.convert_input_to_option(input))
+
+    def convert_param_to_option(self, parameter):
+        """
+        Convert a Parameter into a click Option.
+
+        :type parameter: valohai_yaml.objs.Parameter
+        :rtype: click.Option
+        """
+        assert isinstance(parameter, Parameter)
+        option = click.Option(
+            param_decls=[
+                '--%s' % parameter.name.replace('_', '-'),
+            ],
+            required=(parameter.default is None and not parameter.optional),
+            default=parameter.default,
+            help=parameter.description,
+            type=self.parameter_type_map.get(parameter.type, click.STRING),
+        )
+        option.name = '~%s' % parameter.name  # Tildify so we can pick these out of kwargs easily
+        return option
+
+    def convert_input_to_option(self, input):
+        """
+        Convert an Input into a click Option.
+
+        :type input: valohai_yaml.objs.input.Input
+        :rtype: click.Option
+        """
+        assert isinstance(input, Input)
+        option = click.Option(
+            param_decls=[
+                '--%s' % input.name.replace('_', '-'),
+            ],
+            required=(input.default is None and not input.optional),
+            default=input.default,
+            metavar='URL',
+            help='Input "%s"' % humanize_identifier(input.name),
+        )
+        option.name = '^%s' % input.name  # Caretize so we can pick these out of kwargs easily
+        return option
+
+    def execute(self, **kwargs):
+        """
+        Execute the creation of the execution. (Heh.)
+
+        This is the Click callback for this command.
+
+        :param kwargs: Assorted kwargs (as passed in by Click).
+        :return: Naught
+        """
+        options, parameters, inputs = self._sift_kwargs(kwargs)
+        commit = self.resolve_commit(self.commit)
+        payload = {
+            'commit': commit,
+            'inputs': inputs,
+            'parameters': parameters,
+            'project': self.project.id,
+            'step': self.step.name,
+        }
+        if self.environment:
+            payload['environment'] = self.environment
+        resp = request('post', '/api/v0/executions/', json=payload).json()
+        success('Execution #{counter} created. See {link}'.format(
+            counter=resp['counter'],
+            link=resp['link'],
+        ))
+
+    def _sift_kwargs(self, kwargs):
+        # Sift kwargs into params, options, and inputs
+        options = {}
+        params = {}
+        inputs = {}
+        for key, value in kwargs.items():
+            if key.startswith('~'):
+                params[key[1:]] = value
+            elif key.startswith('^'):
+                inputs[key[1:]] = value
+            else:
+                options[key] = value
+        return (options, params, inputs)
+
+    def resolve_commit(self, commit):
+        if not commit:
+            commit = git.get_current_commit(self.project.directory)
+
+        commits = request('get', '/api/v0/projects/{id}/commits/'.format(id=self.project.id)).json()
+        by_identifier = {c['identifier']: c for c in commits}
+        if commit not in by_identifier:
+            warn('Commit {commit} is not known for the project. Have you pushed it?'.format(commit=commit))
+            raise click.Abort()
+
+        return commit
+
+
+@click.command(context_settings=dict(ignore_unknown_options=True), add_help_option=False)
+@click.argument('step')
+@click.option('--commit', '-c', default=None, metavar='SHA', help='The commit to use. Defaults to the current HEAD.')
+@click.option('--environment/--env', '-e', default=None, help='The environment name/ID to use.')
+@click.argument('args', nargs=-1, type=click.UNPROCESSED)
+@click.pass_context
+def run(ctx, step, commit, environment, args):
+    """
+    Start an execution of a step.
+    """
+    project = get_project(require=True)
+    config = project.get_config()
+    if step == '--help':  # This is slightly weird, but it's because of the nested command thing
+        click.echo(ctx.get_help(), color=ctx.color)
+        ctx.exit()
+    step = match_prefix(config.steps, step)
+    if not step:
+        raise BadParameter(
+            '{step} is not a known step (try one of {steps})'.format(
+                step=step,
+                steps=', '.join(click.style(t, bold=True) for t in sorted(config.steps))
+            ))
+    step = config.steps[step]
+    rc = RunCommand(project, step, commit=commit, environment=environment)
+    with rc.make_context(rc.name, list(args), parent=ctx) as ctx:
+        return rc.invoke(ctx)
