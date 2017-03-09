c047804ec995884794afc26fd57872becbe8686f
Aarni Koskela <akx@iki.fi>
2017-03-09 18:17:09 +0200

    Add a test for get_current_commit

---
+++ tests/test_git.py
@@ -0,0 +1,14 @@
+from subprocess import check_call
+
+from valohai_cli.git import get_current_commit
+
+
+def test_get_current_commit(tmpdir):
+    dir = str(tmpdir)
+    check_call('git init', cwd=dir, shell=True)
+    check_call('git config user.name Robot', cwd=dir, shell=True)
+    check_call('git config user.email robot@example.com', cwd=dir, shell=True)
+    tmpdir.join('test').write_text('test', 'utf8')
+    check_call('git add .', cwd=dir, shell=True)
+    check_call('git commit -mtest', cwd=dir, shell=True)
+    assert len(get_current_commit(dir)) == 40
