de306931e90aefd89f62e98ef6bd91d2288462f0
Aarni Koskela <akx@iki.fi>
2017-03-22 17:01:47 +0200

    Add `project commits` command

---
+++ tests/commands/project/test_commits.py
@@ -0,0 +1,16 @@
+import requests_mock
+
+from tests.fixture_data import EXECUTION_DATA, PROJECT_DATA
+from valohai_cli.commands.project.commits import commits
+
+
+def test_commits(runner, logged_in_and_linked):
+    with requests_mock.mock() as m:
+        commit_data = EXECUTION_DATA['commit']
+        m.get(
+            'https://app.valohai.com/api/v0/projects/{id}/commits/'.format(id=PROJECT_DATA['id']),
+            json=[commit_data],
+        )
+        result = runner.invoke(commits)
+        assert commit_data['ref'] in result.output
+        assert commit_data['identifier'] in result.output
---
+++ valohai_cli/commands/project/commits.py
@@ -0,0 +1,29 @@
+import click
+
+from valohai_cli.api import request
+from valohai_cli.ctx import get_project
+from valohai_cli.git import get_current_commit
+from valohai_cli.messages import print_table
+
+
+@click.command()
+def commits():
+    """
+    List the commits for the linked project.
+    """
+    project = get_project(require=True)
+    commits_data = request('get', '/api/v0/projects/{id}/commits/'.format(id=project.id)).json()
+    try:
+        current_commit = get_current_commit(project.directory)
+    except:
+        current_commit = None
+
+    # Filter out ad-hoc executions (and remove the adhocness marker)
+    commits_data = [commit for commit in commits_data if not commit.pop('adhoc', False)]
+
+    # Mark the current commit
+    for commit in commits_data:
+        if commit['identifier'] == current_commit:
+            commit['identifier'] += ' (current)'
+
+    print_table(commits_data)
