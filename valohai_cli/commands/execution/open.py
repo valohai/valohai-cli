501ef0ea6b43a91379b0cc944c97883cc2bebd18
Aarni Koskela <akx@iki.fi>
2017-04-13 15:31:36 +0300

    Add `exec open` and `proj open` utilities

---
+++ valohai_cli/commands/execution/open.py
@@ -0,0 +1,14 @@
+import click
+
+from valohai_cli.ctx import get_project
+from valohai_cli.utils import open_browser
+
+
+@click.command()
+@click.argument('counter')
+def open(counter):
+    """
+    Open an execution in a web browser.
+    """
+    execution = get_project(require=True).get_execution_from_counter(counter=counter, detail=True)
+    open_browser(execution)
---
+++ valohai_cli/commands/project/open.py
@@ -0,0 +1,15 @@
+import click
+
+from valohai_cli.api import request
+from valohai_cli.ctx import get_project
+from valohai_cli.utils import open_browser
+
+
+@click.command()
+def open():
+    """
+    Open the project's view in a web browser.
+    """
+    project = get_project(require=True)
+    project_data = request('get', '/api/v0/projects/{id}/'.format(id=project.id)).json()
+    open_browser(project_data)
