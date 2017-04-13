b6441f7b3605807da849028a74b02498854f6f56
Aarni Koskela <akx@iki.fi>
2017-04-13 10:38:08 +0300

    Add `project list` command

---
+++ tests/commands/project/test_list.py
@@ -0,0 +1,11 @@
+import requests_mock
+
+from tests.fixture_data import PROJECT_DATA
+from valohai_cli.commands.project.list import list
+
+
+def test_list(runner, logged_in):
+    with requests_mock.mock() as m:
+        m.get('https://app.valohai.com/api/v0/projects/', json={'results': [PROJECT_DATA]})
+        result = runner.invoke(list)
+        assert PROJECT_DATA['name'] in result.output
