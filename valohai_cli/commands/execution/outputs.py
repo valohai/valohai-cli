500773a40c08f7d1a7df75c82b730405dcce41ee
Aarni Koskela <akx@iki.fi>
2017-09-18 14:39:11 +0300

    Add --filter-download

--- valohai_cli/commands/execution/outputs.py
+++ valohai_cli/commands/execution/outputs.py
@@ -1,5 +1,6 @@
 import os
 import time
+from fnmatch import fnmatch
 
 import click
 import requests
@@ -12,8 +13,9 @@ from valohai_cli.utils import force_text
 @click.command()
 @click.argument('counter')
 @click.option('--download', '-d', type=click.Path(file_okay=False),
-              help='download files to this directory', default=None)
-def outputs(counter, download):
+              help='download files to this directory (by default, don\'t download)', default=None)
+@click.option('--filter-download', '-f', help='download only files matching this glob', default=None)
+def outputs(counter, download, filter_download):
     """
     List and download execution outputs.
     """
@@ -24,10 +26,15 @@ def outputs(counter, download):
         return
     print_table(outputs, ('name', 'url', 'size'))
     if download:
+        if filter_download:
+            outputs = [output for output in outputs if fnmatch(output['name'], filter_download)]
         download_outputs(outputs, download)
 
 
 def download_outputs(outputs, output_path):
+    if not outputs:
+        warn('Nothing to download.')
+        return
     total_size = sum(o['size'] for o in outputs)
     num_width = len(str(len(outputs)))  # How many digits required to print the number of outputs
     start_time = time.time()
