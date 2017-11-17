da9049785d946f281ae1d3dfbf1d3847192a3d0b
Aarni Koskela <akx@iki.fi>
2017-11-17 13:56:10 +0200

    exec run: Rephrase -e

--- valohai_cli/commands/execution/run.py
+++ valohai_cli/commands/execution/run.py
@@ -34,7 +34,7 @@ class RunCommand(click.Command):
         :type step: valohai_yaml.objs.Step
         :param commit: Commit identifier
         :type commit: str
-        :param environment: Environment name
+        :param environment: Environment identifier (slug or UUID)
         :type environment: str
         :param watch: Whether to chain to `exec watch` afterwards
         :type watch: bool
@@ -155,7 +155,7 @@ class RunCommand(click.Command):
 @click.command(context_settings=dict(ignore_unknown_options=True), add_help_option=False)
 @click.argument('step')
 @click.option('--commit', '-c', default=None, metavar='SHA', help='The commit to use. Defaults to the current HEAD.')
-@click.option('--environment', '-e', default=None, help='The environment UUID to use.')
+@click.option('--environment', '-e', default=None, help='The environment UUID or slug to use.')
 @click.option('--watch', '-w', is_flag=True, help='Start `exec watch`ing the execution after it starts')
 @click.option(
     '--adhoc',
