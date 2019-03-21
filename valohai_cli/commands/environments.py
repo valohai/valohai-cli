fa3c40fce7ffbec9d64b16bea899b0043d1e8cf7
Juha Kiili <juha@juhakiili.com>
2019-03-21 14:37:40 +0200

    Add --gpu flag for vh environments to show GPU specifications

--- valohai_cli/commands/environments.py
+++ valohai_cli/commands/environments.py
@@ -7,17 +7,26 @@ from valohai_cli.table import print_table
 
 
 @click.command()
-def environments():
+@click.option('--gpu', '-g', is_flag=True, help='Show GPU specifications.')
+def environments(gpu):
     """
     List all available execution environments.
     """
     envs_data = request('get', '/api/v0/environments/', params={'limit': 9000}).json()['results']
     envs_data.sort(key=itemgetter('name'))
+
+    columns = ['name', 'slug', 'description', 'per_hour_price_usd', 'per_user_queue_quota', 'unfinished_job_count']
+    headers = ['Name', 'Slug', 'Description', 'Per-Hour USD$', 'Per-User Quota', 'Jobs in Queue']
+
+    if gpu:
+        columns = ['name', 'gpu_spec', 'per_hour_price_usd', 'per_user_queue_quota', 'unfinished_job_count']
+        headers = ['Name', 'GPU Specification', 'Per-Hour USD$', 'Per-User Quota', 'Jobs in Queue']
+
     for env in envs_data:
         if 'per_user_queue_quota' in env and env['per_user_queue_quota'] <= 0:
             env['per_user_queue_quota'] = 'unlimited'
     print_table(
         data=envs_data,
-        columns=['name', 'slug', 'description', 'per_hour_price_usd', 'per_user_queue_quota', 'unfinished_job_count'],
-        headers=['Name', 'Slug', 'Description', 'Per-Hour USD$', 'Per-User Quota', 'Jobs in Queue'],
+        columns=columns,
+        headers=headers,
     )
