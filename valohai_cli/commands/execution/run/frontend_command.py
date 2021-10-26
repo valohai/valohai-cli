valohai/valohai-cli

commit 1c5141ef75ee44d70f739240825ac9300199fcb7

Extract commit resolution logic in execution creation

valohai_cli/commands/execution/run/frontend_command.py
@@ -3,10 +3,10 @@ from typing import Any, List, Optional
 
 import click
 
-from valohai_cli.adhoc import package_adhoc_commit
 from valohai_cli.ctx import get_project
 from valohai_cli.messages import info
 from valohai_cli.utils import parse_environment_variable_strings
+from valohai_cli.utils.commits import create_or_resolve_commit
 
 from .dynamic_run_command import RunCommand
 from .utils import match_step
@@ -63,12 +63,6 @@ def run(
         return
     project = get_project(require=True)
 
-    if adhoc:
-        if commit:
-            raise click.UsageError('--commit and --adhoc are mutually exclusive.')
-        if project.is_remote:
-            raise click.UsageError('--adhoc can not be used with remote projects.')
-
     if download_directory and watch:
         raise click.UsageError('Combining --sync and --watch not supported yet.')
 
@@ -87,6 +81,8 @@ def run(
     matched_step = match_step(config, step_name)
     step = config.steps[matched_step]
 
+    commit = create_or_resolve_commit(project, commit=commit, adhoc=adhoc)
+
     rc = RunCommand(
         project=project,
         step=step,
@@ -100,8 +96,6 @@ def run(
         tags=tags,
     )
     with rc.make_context(rc.name, list(args), parent=ctx) as child_ctx:
-        if adhoc:
-            rc.commit = package_adhoc_commit(project, validate=validate_adhoc)['identifier']
         return rc.invoke(child_ctx)
 
 

valohai_cli/utils/commits.py
@@ -1,0 +1,61 @@
+from typing import Optional, Union
+
+import click
+
+from valohai_cli import git as git
+from valohai_cli.adhoc import package_adhoc_commit
+from valohai_cli.exceptions import NoGitRepo
+from valohai_cli.messages import warn
+from valohai_cli.models.project import Project
+from valohai_cli.models.remote_project import RemoteProject
+
+
+def create_or_resolve_commit(
+    project: Union[RemoteProject, Project],
+    *,
+    commit: Optional[str],
+    adhoc: bool,
+) -> str:
+    if adhoc:
+        if project.is_remote:
+            raise click.UsageError('--adhoc can not be used with remote projects.')
+        if commit:
+            raise click.UsageError('--commit and --adhoc are mutually exclusive.')
+        commit = str(package_adhoc_commit(project, validate=True)['identifier'])
+
+    commit = commit or get_git_commit(project)
+    return resolve_commit(commit, project)
+
+
+def get_git_commit(project: Project) -> Optional[str]:
+    try:
+        return git.get_current_commit(project.directory)
+    except NoGitRepo:
+        warn(
+            'The directory is not a Git repository. \n'
+            'Would you like to just run using the latest commit known by Valohai?'
+        )
+        if not click.confirm('Use latest commit?', default=True):
+            raise click.Abort()
+        return None
+
+
+def resolve_commit(commit_identifier: Optional[str], project: Project) -> str:
+    if commit_identifier and commit_identifier.startswith('~'):
+        # Assume ad-hoc commits are qualified already
+        return commit_identifier
+
+    try:
+        commit_obj = project.resolve_commit(commit_identifier=commit_identifier)
+    except KeyError:
+        warn(f'Commit {commit_identifier} is not known for the project. Have you pushed it?')
+        raise click.Abort()
+    except IndexError:
+        warn('No commits are known for the project.')
+        raise click.Abort()
+
+    resolved_commit_identifier: str = commit_obj['identifier']
+    if not commit_identifier:
+        click.echo(f'Resolved to commit {resolved_commit_identifier}', err=True)
+
+    return resolved_commit_identifier

tests/commands/execution/test_run.py
@@ -5,6 +5,7 @@ from tests.commands.run_test_utils import RunAPIMock, RunTestSetup
 from tests.fixture_data import CONFIG_YAML, PROJECT_DATA
 from valohai_cli.commands.execution.run import run
 from valohai_cli.ctx import get_project
+from valohai_cli.models.project import Project
 
 adhoc_mark = pytest.mark.parametrize('adhoc', (False, True), ids=('regular', 'adhoc'))
 
@@ -12,6 +13,14 @@ adhoc_mark = pytest.mark.parametrize('adhoc', (False, True), ids=('regular', 'ad
 @pytest.fixture(params=['regular', 'adhoc'], ids=('regular', 'adhoc'))
 def run_test_setup(request, logged_in_and_linked, monkeypatch):
     return RunTestSetup(monkeypatch=monkeypatch, adhoc=(request.param == 'adhoc'))
+
+
+@pytest.fixture()
+def patch_git(monkeypatch):
+    def mock_resolve_commit(mock_self, *, commit_identifier):
+        return {'identifier': commit_identifier}
+
+    monkeypatch.setattr(Project, 'resolve_commit', mock_resolve_commit)
 
 
 def test_run_requires_step(runner, logged_in_and_linked):
@@ -77,7 +86,7 @@ def test_run_params(tmpdir, run_test_setup, pass_param):
     run_test_setup.run()
 
 
-def test_param_type_validation_integer(runner, logged_in_and_linked):
+def test_param_type_validation_integer(runner, logged_in_and_linked, patch_git):
     with open(get_project().get_config_filename(), 'w') as yaml_fp:
         yaml_fp.write(CONFIG_YAML)
     rv = runner.invoke(run, ['train', '--max-steps=plonk'], catch_exceptions=False)
@@ -87,7 +96,7 @@ def test_param_type_validation_integer(runner, logged_in_and_linked):
     )
 
 
-def test_param_type_validation_flag(runner, logged_in_and_linked):
+def test_param_type_validation_flag(runner, logged_in_and_linked, patch_git):
     with open(get_project().get_config_filename(), 'w') as yaml_fp:
         yaml_fp.write(CONFIG_YAML)
     rv = runner.invoke(run, ['train', '--enable-mega-boost=please'], catch_exceptions=False)
@@ -128,7 +137,7 @@ def test_run_no_git(runner, logged_in_and_linked):
         assert 'is not a Git repository' in output
 
 
-def test_param_input_sanitization(runner, logged_in_and_linked):
+def test_param_input_sanitization(runner, logged_in_and_linked, patch_git):
     with open(get_project().get_config_filename(), 'w') as yaml_fp:
         yaml_fp.write('''
 - step:
@@ -151,7 +160,7 @@ def test_param_input_sanitization(runner, logged_in_and_linked):
     assert '--ridiculously-complex-input-name' in output
 
 
-def test_typo_check(runner, logged_in_and_linked):
+def test_typo_check(runner, logged_in_and_linked, patch_git):
     with open(get_project().get_config_filename(), 'w') as yaml_fp:
         yaml_fp.write(CONFIG_YAML)
     args = ['train', '--max-setps=80']  # Oopsy!
@@ -168,7 +177,7 @@ def test_run_help(runner, logged_in_and_linked):
     assert 'Train model' in output
 
 
-def test_command_help(runner, logged_in_and_linked):
+def test_command_help(runner, logged_in_and_linked, patch_git):
     with open(get_project().get_config_filename(), 'w') as yaml_fp:
         yaml_fp.write(CONFIG_YAML)
 

valohai_cli/commands/execution/run/dynamic_run_command.py
@@ -10,9 +10,8 @@ from valohai_yaml.objs.input import Input
 from valohai_yaml.objs.parameter import Parameter
 from valohai_yaml.utils import listify
 
-from valohai_cli import git as git
 from valohai_cli.api import request
-from valohai_cli.exceptions import CLIException, NoGitRepo
+from valohai_cli.exceptions import CLIException
 from valohai_cli.messages import success, warn
 from valohai_cli.models.project import Project
 from valohai_cli.utils import humanize_identifier, sanitize_option_name
@@ -49,7 +48,7 @@ class RunCommand(click.Command):
         self,
         project: Project,
         step: Step,
-        commit: Optional[str],
+        commit: str,
         environment: Optional[str] = None,
         image: Optional[str] = None,
         title: Optional[str] = None,
@@ -158,10 +157,9 @@ class RunCommand(click.Command):
         :return: Naught
         """
         options, parameters, inputs = self._sift_kwargs(kwargs)
-        commit = self.resolve_commit(self.commit)
 
         payload = {
-            'commit': commit,
+            'commit': self.commit,
             'inputs': inputs,
             'parameters': parameters,
             'project': self.project.id,
@@ -251,37 +249,6 @@ class RunCommand(click.Command):
                     missing_required_parameters.add(name)
         if missing_required_parameters:
             raise CLIException(f'Required parameters missing: {missing_required_parameters}')
-
-    def resolve_commit(self, commit_identifier: Optional[str]) -> str:
-        if not commit_identifier:
-            try:
-                commit_identifier = git.get_current_commit(self.project.directory)
-            except NoGitRepo:
-                warn(
-                    'The directory is not a Git repository. \n'
-                    'Would you like to just run using the latest commit known by Valohai?'
-                )
-                if not click.confirm('Use latest commit?', default=True):
-                    raise click.Abort()
-
-        if commit_identifier and commit_identifier.startswith('~'):
-            # Assume ad-hoc commits are qualified already
-            return commit_identifier
-
-        try:
-            commit_obj = self.project.resolve_commit(commit_identifier=commit_identifier)
-        except KeyError:
-            warn(f'Commit {commit_identifier} is not known for the project. Have you pushed it?')
-            raise click.Abort()
-        except IndexError:
-            warn('No commits are known for the project.')
-            raise click.Abort()
-
-        resolved_commit_identifier: str = commit_obj['identifier']
-        if not commit_identifier:
-            click.echo(f'Resolved to commit {resolved_commit_identifier}', err=True)
-
-        return resolved_commit_identifier
 
     def make_parser(self, ctx: Context) -> FriendlyOptionParser:
         parser: FriendlyOptionParser = super().make_parser(ctx)  # type: ignore[assignment]

commit 983e4aeb571c29cdd01f0eaf10e839cfa7538d5d

Restore --no-validate-adhoc to exec run
Regressed in 1c5141ef75ee44d70f739240825ac9300199fcb7

valohai_cli/commands/execution/run/frontend_command.py
@@ -81,7 +81,12 @@ def run(
     matched_step = match_step(config, step_name)
     step = config.steps[matched_step]
 
-    commit = create_or_resolve_commit(project, commit=commit, adhoc=adhoc)
+    commit = create_or_resolve_commit(
+        project,
+        commit=commit,
+        adhoc=adhoc,
+        validate_adhoc_commit=validate_adhoc,
+    )
 
     rc = RunCommand(
         project=project,

valohai_cli/utils/commits.py
@@ -15,13 +15,14 @@ def create_or_resolve_commit(
     *,
     commit: Optional[str],
     adhoc: bool,
+    validate_adhoc_commit: bool = True,
 ) -> str:
     if adhoc:
         if project.is_remote:
             raise click.UsageError('--adhoc can not be used with remote projects.')
         if commit:
             raise click.UsageError('--commit and --adhoc are mutually exclusive.')
-        commit = str(package_adhoc_commit(project, validate=True)['identifier'])
+        commit = str(package_adhoc_commit(project, validate=validate_adhoc_commit)['identifier'])
 
     commit = commit or get_git_commit(project)
     return resolve_commit(commit, project)