fd90d659fc842e5c37c60d30e6ac40c03f9743a4
Aarni Koskela <akx@iki.fi>
2021-12-21 13:42:37 +0200

    Use valohai-yaml 0.21+ pipeline conversion code

--- valohai_cli/commands/pipeline/run/utils.py
+++ valohai_cli/commands/pipeline/run/utils.py
@@ -1,60 +1,7 @@
-from typing import List
-
 import click
-from valohai_yaml.objs import Config, ExecutionNode, Pipeline, Step
+from valohai_yaml.objs import Config
 
 from valohai_cli.utils import match_prefix
-
-
-def build_nodes(commit: str, config: Config, pipeline: Pipeline) -> List[dict]:
-    nodes = []
-    for node in pipeline.nodes:
-        if isinstance(node, ExecutionNode):
-            step = config.steps[node.step]
-            template = build_node_template(commit, step)
-            nodes.append({
-                "name": node.name,
-                "type": node.type,
-                "template": template,
-            })
-            continue
-        raise NotImplementedError(f"{node.type} nodes are not supported by the CLI at present")
-    return nodes
-
-
-def build_node_template(commit: str, step: Step) -> dict:
-    template = {
-        "commit": commit,
-        "step": step.name,
-        "image": step.image,
-        "command": step.command,
-        "inputs": {
-            name: (input.default or []) for (name, input) in step.inputs.items()
-        },
-        "parameters": {
-            key: step.parameters[key].default for key in
-            list(step.parameters)
-        },
-        "inherit_environment_variables": True,
-        "environment_variables": dict(step.environment_variables),
-    }
-    if step.environment:
-        template["environment"] = step.environment
-    return template
-
-
-def build_edges(pipeline: Pipeline) -> List[dict]:
-    return [
-        {
-            "source_node": edge.source_node,
-            "source_key": edge.source_key,
-            "source_type": edge.source_type,
-            "target_node": edge.target_node,
-            "target_type": edge.target_type,
-            "target_key": edge.target_key,
-        }
-        for edge in pipeline.edges
-    ]
 
 
 def match_pipeline(config: Config, pipeline_name: str) -> str:
