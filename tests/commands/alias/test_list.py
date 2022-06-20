67e9660b5a5090f6dea51778dca4f59b9ee9cd82
Tomi Kokkonen <tomi@valohai.com>
2022-06-20 10:40:14 +0300

    Add data and alias command to fetch Datums and Datum Aliases

---
+++ tests/commands/alias/test_list.py
@@ -0,0 +1,22 @@
+import requests_mock
+
+from tests.fixture_data import DATUM_ALIAS_DATA
+from valohai_cli.commands.alias.list import list
+
+
+def test_list(runner, logged_in_and_linked):
+
+    with requests_mock.mock() as m:
+        m.get(
+            'https://app.valohai.com/api/v0/datum-aliases/',
+            json={
+                'results': [
+                    DATUM_ALIAS_DATA,
+                    DATUM_ALIAS_DATA,
+                    DATUM_ALIAS_DATA,
+                ],
+            },
+        )
+
+        output = runner.invoke(list, catch_exceptions=False).output
+        assert output.count('datum://this-is-alias-for-latest-png') == 3  # Three times the url
---
+++ tests/commands/data/test_list.py
@@ -0,0 +1,21 @@
+import requests_mock
+
+from tests.fixture_data import DATUM_DATA
+from valohai_cli.commands.data.list import list
+
+
+def test_list(runner, logged_in_and_linked):
+    with requests_mock.mock() as m:
+        m.get(
+            'https://app.valohai.com/api/v0/data/',
+            json={
+                'results': [
+                    DATUM_DATA,
+                    DATUM_DATA,
+                    DATUM_DATA,
+                ],
+            },
+        )
+
+        output = runner.invoke(list, catch_exceptions=False).output
+        assert output.count(f'datum://{DATUM_DATA["id"]}') == 3  # Three times the url
