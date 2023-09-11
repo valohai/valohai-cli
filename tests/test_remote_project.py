29850432f4642b35b4544f4aed2302ccbcca70ab
Aarni Koskela <akx@iki.fi>
2023-09-11 14:06:20 +0300

    Wrap remote_project_setup in a fixture for DRY

--- tests/test_remote_project.py
+++ tests/test_remote_project.py
@@ -1,3 +1,5 @@
+import pytest
+
 from tests.commands.run_test_utils import RunAPIMock
 from tests.fixture_data import PROJECT_DATA
 from valohai_cli.commands.execution.run import run as exec_run
@@ -9,7 +11,8 @@ from valohai_cli.settings import settings
 test_token = 'x' * 12
 
 
-def test_execution_remote_project(request, runner, monkeypatch, tmpdir):
+@pytest.fixture()
+def remote_project_setup(request, monkeypatch, tmpdir):
     project_id = PROJECT_DATA['id']
     request.addfinalizer(lambda: settings.reset())
     monkeypatch.chdir(tmpdir)
@@ -17,27 +20,28 @@ def test_execution_remote_project(request, runner, monkeypatch, tmpdir):
         configure_token_login(None, test_token)
         configure_project_override(project_id=project_id, mode=None)
         assert isinstance(settings.override_project, RemoteProject)
-        # Regular execution
-        run_output = runner.invoke(exec_run, ['Train model'], catch_exceptions=False).output
-        assert 'created' in run_output
-        assert 'Using remote project' in run_output
-        # Ad-hocs don't work
-        assert 'can not be used' in runner.invoke(exec_run, ['Train model', '--adhoc'], catch_exceptions=False).output
+        yield
 
 
-def test_pipeline_remote_project(request, runner, monkeypatch, tmpdir):
-    project_id = PROJECT_DATA['id']
-    request.addfinalizer(lambda: settings.reset())
-    monkeypatch.chdir(tmpdir)
-    with RunAPIMock(project_id, 'f' * 40, {}):
-        configure_token_login(None, test_token)
-        configure_project_override(project_id=project_id, mode=None)
-        assert isinstance(settings.override_project, RemoteProject)
-        # Regular execution
-        run_output = runner.invoke(pipeline_run, ['Train Pipeline', f'--commit={"f" * 16}'],
-                                   catch_exceptions=False).output
-        assert 'queued' in run_output
-        assert 'Success! Pipeline =' in run_output
-        # Ad-hocs don't work
-        run_output = runner.invoke(pipeline_run, ['Training Pipeline', '--adhoc'], catch_exceptions=False).output
-        assert 'can not be used' in run_output
+def test_execution_remote_project(remote_project_setup, runner):
+    run_output = runner.invoke(exec_run, ['Train model'], catch_exceptions=False).output
+    assert 'created' in run_output
+    assert 'Using remote project' in run_output
+
+
+def test_execution_remote_project_adhoc(remote_project_setup, runner):
+    # Ad-hocs don't work with remote projects
+    run_output = runner.invoke(exec_run, ['Train model', '--adhoc'], catch_exceptions=False).output
+    assert 'can not be used' in run_output
+
+
+def test_pipeline_remote_project(remote_project_setup, runner):
+    run_output = runner.invoke(pipeline_run, ['Train Pipeline', f'--commit={"f" * 16}'], catch_exceptions=False).output
+    assert 'queued' in run_output
+    assert 'Success! Pipeline =' in run_output
+
+
+def test_pipeline_remote_project_adhoc(remote_project_setup, runner):
+    # Ad-hocs don't work with remote projects
+    run_output = runner.invoke(pipeline_run, ['Training Pipeline', '--adhoc'], catch_exceptions=False).output
+    assert 'can not be used' in run_output
