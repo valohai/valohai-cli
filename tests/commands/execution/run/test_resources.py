1dc320e2e9ee61d3e7e8260b8d65895bab8ad17b
Ruksi Korpisara <me@ruk.si>
2025-12-04 15:10:04 +0200

    Split remaining k8s param tests
    ... to resources focused and preset focused ones

--- tests/commands/execution/run/test_resources.py
+++ tests/commands/execution/run/test_resources.py
@@ -137,19 +137,3 @@ def test_kube_step_override_device_empty(run_test_setup_resources: RunTestSetup)
             },
         },
     }
-
-
-def test_kube_runtime_config_preset_argument(run_test_setup_resources: RunTestSetup):
-    preset_uuid = "yes-this-is-my-preset-uuid"
-    run_test_setup_resources.args.append(f"--k8s-preset={preset_uuid}")
-    run_test_setup_resources.run()
-
-    payload = run_test_setup_resources.run_api_mock.last_create_execution_payload
-    assert payload and payload["runtime_config_preset"] == preset_uuid
-
-
-def test_kube_no_runtime_config_preset_argument(run_test_setup_resources: RunTestSetup):
-    """Only add the preset to payload when explicitly specified."""
-    run_test_setup_resources.run()
-    payload = run_test_setup_resources.run_api_mock.last_create_execution_payload
-    assert payload and "runtime_config_preset" not in payload
